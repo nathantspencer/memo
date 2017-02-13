748a29f9cb6be1955ce2d3da26fde026b31b8b27
nathantspencer <nathantspencer@gmail.com>
2017-02-12 23:21:22 -0500

    Initial Commit

---
+++ memo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  memo
+//
+//  Created by Nathan Spencer on 2/12/17.
+//  Copyright © 2017 Nathan Spencer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
