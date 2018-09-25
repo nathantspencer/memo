00ffd05cd873aa801800e2b71428ca77c505e84d
nathantspencer <nathantspencer@gmail.com>
2018-09-25 08:24:59 -0400

    Experimenting with curses loop.
    Switched to different PDCurses fork.
    
    Revert "Switched to different PDCurses fork."
    
    This reverts commit 9400e2078fab53a54c010b2a3bd8ff2c9b3e9e03.
    
    Experimenting with polling console window size, win32.
    
    Added skeleton of state classes.

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,8 +2,13 @@
 #include <iostream>
 
 #include <curses.h>
+#include <string>
 
-#define VERSION "0.1.0"
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
+constexpr char* VERSION = "0.1.0";
 
 int main(int argc, char* argv[])
 {
@@ -14,7 +19,46 @@ int main(int argc, char* argv[])
 
 		if (argument == "new")
 		{
-			// TODO
+			initscr();
+			raw();
+			keypad(stdscr, TRUE);
+			noecho();
+
+#ifdef WIN32
+			halfdelay(5);
+#endif
+
+			WINDOW* dateTimePanel = newwin(LINES - 2, 8, 1, 2);
+
+			do
+			{
+				refresh();
+#ifdef WIN32
+				int columns, rows;
+
+				CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+				auto consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+				bool x = consoleHandle == INVALID_HANDLE_VALUE;
+				GetConsoleScreenBufferInfo(consoleHandle, &consoleInfo);
+
+				columns = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+				rows = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+#endif
+				if (COLS > 20)
+				{
+					mvwprintw(dateTimePanel, 0, 0, (" 9." + std::to_string(consoleInfo.dwSize.X)).c_str());
+					mvwprintw(dateTimePanel, 1, 0, "10:03 PM");
+					wrefresh(dateTimePanel);
+				}
+				else
+				{
+					wprintw(dateTimePanel, "derp");
+				}
+			} while (getch());
+
+			endwin();
+
+			return 0;
 		}
 		else if (argument == "view")
 		{
