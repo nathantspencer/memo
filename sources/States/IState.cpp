140519a0ec2d86ed7caae8dd02a6d57ba180144d
nathantspencer <nathantspencer@gmail.com>
2018-10-31 19:09:50 -0400

    Tweaks to IState API

--- sources/States/IState.cpp
+++ sources/States/IState.cpp
@@ -1,17 +1,27 @@
 #include "IState.h"
 
-IState::IState(std::function<void()> terminateHandler, std::function<void(IStatePtr)> addChildStateHandler) :
-	m_terminateHandler(terminateHandler),
-	m_addChildStateHandler(addChildStateHandler)
+void IState::SetTerminateHandler(std::function<void()> terminateHandler)
 {
+	m_terminateHandler = terminateHandler;
+}
+
+void IState::SetAddChildStateHandler(std::function<void(IStatePtr)> addChildStateHandler)
+{
+	m_addChildStateHandler = addChildStateHandler;
 }
 
 void IState::Terminate()
 {
-	m_terminateHandler();
+	if (m_terminateHandler)
+	{
+		m_terminateHandler();
+	}
 }
 
 void IState::AddChildState(IStatePtr childState)
 {
-	m_addChildStateHandler(childState);
+	if (m_addChildStateHandler)
+	{
+		m_addChildStateHandler(childState);
+	}
 }
--- sources/States/IState.h
+++ sources/States/IState.h
@@ -12,29 +12,33 @@ typedef std::shared_ptr<IState> IStatePtr;
 class IState
 {
 public:
-	/*! Constructor for `IState`. Requires two functions as arguments: one that handles
-	 *  the constructed state's termination and another that handles it's creation of
-	 *  child states.
+	/*! Adds a callback for the state's termination. Useful for classes that manage states.
 	 *
 	 *  @param  terminateHandler  callback for the state's termination
+	 */
+	void SetTerminateHandler(std::function<void()> terminateHandler);
+
+	/*! Adds a callback used when the state attempts to temporarily delegate execution to
+	 *  a child state. Useful for classes that manage states.
+	 *
 	 *  @param  addChildStateHandler  callback for the creation of child states
 	 */
-	IState(std::function<void()> terminateHandler, std::function<void(IStatePtr)> addChildStateHandler);
+	void SetAddChildStateHandler(std::function<void(IStatePtr)> addChildStateHandler);
 
 	/*! Begins the execution of the state.
 	 */
 	virtual void Execute() = 0;
 
 protected:
-	/*! Finishes things up and calls the state's termination handler. Should be called
-	 *  by all implementers when the state's execution is done.
+	/*! Finishes things up and calls the state's termination handler if one exists.
+	 *  Should be called by all implementers when the state's execution is done.
 	 */
 	virtual void Terminate();
 
-	/*! Calls the add child state handler, which should pass off execution to a child
-	 *  state temporarily.
+	/*! Calls the add child state handler if one exists, attempting to temporarily
+	 *  pass off execution to another 'child' state.
 	 *
-	 *  @param  childState  a new state which will become a child of the calling state
+	 *  @param  childState  state to become a child of the calling state
 	 */
 	virtual void AddChildState(IStatePtr childState);
 
