10987cd5cb0c4a4850e51ae6326fd82d2faa7177
nathantspencer <nathantspencer@gmail.com>
2018-11-02 13:55:23 -0400

    Added MoveCursor function to wrap cursor movement over lines. Started backspace.

--- sources/States/NewState/UniversalNewState.cpp
+++ sources/States/NewState/UniversalNewState.cpp
@@ -4,6 +4,40 @@
 
 #include <curses.h>
 
+enum CursorDirection { UP, DOWN, LEFT, RIGHT };
+
+/// -- Moves the cursor by one character in the given direction/panel
+/// -- Wraps over the ends of lines when moving left/right
+static void MoveCursor(WINDOW* panel, CursorDirection direction)
+{
+	if (direction == UP || direction == DOWN)
+	{
+		wmove(panel, getcury(panel) + !!(direction == UP), getcurx(panel) + !!(direction == DOWN));
+	}
+	else if (direction == LEFT)
+	{
+		if (!getcurx(panel) && !!getcury(panel))
+		{
+			wmove(panel, getcury(panel) - 1, panel->_maxx - 1);
+		}
+		else
+		{
+			wmove(panel, getcury(panel), getcurx(panel) - 1);
+		}
+	}
+	else if (direction == RIGHT)
+	{
+		if (getcurx(panel) == panel->_maxx - 1 && getcury(panel) != panel->_maxx)
+		{
+			wmove(panel, getcury(panel) + 1, 0);
+		}
+		else
+		{
+			wmove(panel, getcury(panel), getcurx(panel) + 1);
+		}
+	}
+}
+
 void UniversalNewState::Execute()
 {
 	initscr();
@@ -28,12 +62,28 @@ void UniversalNewState::Execute()
 		mvwprintw(dateTimePanel, 1, 0, "10:03 PM");
 		mvwvline(dateTimePanel, 0, 10, ACS_VLINE, LINES - 2);
 
-		if (key == KEY_UP || key == KEY_DOWN || key == KEY_LEFT || key == KEY_RIGHT)
+		if (key == KEY_UP)
 		{
-			std::pair<int, int> cursorOffset;
-			cursorOffset.first = (key == KEY_UP ? -1 : 0) + (key == KEY_DOWN ? 1 : 0);
-			cursorOffset.second = (key == KEY_LEFT ? -1 : 0) + (key == KEY_RIGHT ? 1 : 0);
-			wmove(textPanel, getcury(textPanel) + cursorOffset.first, getcurx(textPanel) + cursorOffset.second);
+			MoveCursor(textPanel, UP);
+		}
+		else if (key == KEY_DOWN)
+		{
+			MoveCursor(textPanel, DOWN);
+		}
+		else if (key == KEY_LEFT)
+		{
+			MoveCursor(textPanel, LEFT);
+		}
+		else if (key == KEY_RIGHT)
+		{
+			MoveCursor(textPanel, RIGHT);
+		}
+		else if (key == 8) // TODO: fix this hack for PC backspace
+		{
+			MoveCursor(textPanel, LEFT);
+			waddch(textPanel, ' '); // TODO: is there a better empty character that can be used?
+			MoveCursor(textPanel, LEFT);
+			// TODO: backspace should shift all trailing characters to the left
 		}
 		else if (key != KEY_RESIZE && key != ERR)
 		{
